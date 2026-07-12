f7500a05659302c576f5357b93403507de1095bd
Ritamausi <rita.schuett33@gmail.com>
2026-07-12 23:28:18 +0200

    Add Telegram bot to check for available appointments

---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+import time
+import requests
+from bs4 import BeautifulSoup
+from telegram import Bot
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+URL ="https://scheduler.clinicore.eu/drkultas?showCards=true"
+
+bot = Bot(token=BOT_TOKEN)
+
+def check():
+    response = requests.get(URL, timeout=30)
+    text = response.text.lower()
+
+    suchbegriffe = [
+        "termin verfügbar",
+        "verfügbar",
+        "freie termine",
+        "auswählen"
+    ]
+
+    for wort in suchbegriffe:
+        if wort in text:
+            bot.send_message(
+                chat_id=CHAT_ID,
+                text="🚨 Es könnte ein freier Clinicore-Termin verfügbar sein!"
+            )
+            return True
+
+    return False
+
+
+while True:
+    try:
+        check()
+    except Exception as e:
+        print(e)
+
+    time.sleep(120)
